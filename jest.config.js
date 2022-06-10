a9c229bfee4080ad8f776d2ebe92026fa9dd9753
kim <rlaruddnjs28@gmail.com>
2022-06-10 13:44:08 +0900

    feat(jest): add module alias

--- jest.config.js
+++ jest.config.js
@@ -11,6 +11,7 @@ const customJestConfig = {
   moduleNameMapper: {
     // Handle module aliases (this will be automatically configured for you soon)
     '^@/components/(.*)$': '<rootDir>/components/$1',
+    '^@/lib/(.*)$': '<rootDir>/lib/$1',
     '^@/pages/(.*)$': '<rootDir>/pages/$1',
   },
   testEnvironment: 'jest-environment-jsdom',
